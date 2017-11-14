fae22405a7513921e8d51d0b2a4ce0c8e0970c5b
Stavros Pitoglou <s.pitoglou@csl.gr>
2017-11-14 14:46:10 +0200

    Add input for tests

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { Actions } from 'react-native-router-flux'
 
 const AboutPage = () => {
   return (
-    <View style={{ margin: 128 }}>
+    <View style={{ alignSelf: 'center' }}>
       <Text onPress={Actions.mainPage}>Go to main Page (Scanner)</Text>
+      <TextInput />
     </View>
   )
 };
