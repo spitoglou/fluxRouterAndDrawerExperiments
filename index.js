1791c38a1140be334875d1a5e24b644f8b25a2d4
Stavros Pitoglou <s.pitoglou@csl.gr>
2017-11-14 16:02:10 +0200

    At last!!!

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 import { AppRegistry } from 'react-native';
 import React, { Component } from 'react'
-import { Router, Scene } from 'react-native-router-flux';
+import { Drawer, Router, Scene } from 'react-native-router-flux';
 import MainPage from './src/pages/MainPage';
 import AboutPage from './src/pages/AboutPage';
-import SideBar from './src/components/SideBar'
+import Menu from './src/components/Menu'
+import MenuIcon from './images/menu_burger.png';
 
 class App extends Component {
   render() {
     return (
-      <SideBar>
-        <Router>
-          <Scene key="root" hideNavBar>
-            <Scene key="mainPage" component={MainPage} hideNavBar title="Home" />
+
+      <Router>
+        <Drawer
+          hideNavBar
+          key="drawer"
+          contentComponent={Menu}
+          drawerWidth={300}
+          menuIcon={MenuIcon}
+        >
+          <Scene key="root">
+            <Scene key="mainPage" component={MainPage} title="Home" />
             <Scene key="aboutPage" component={AboutPage} title="About" initial={true} />
           </Scene>
-        </Router>
-      </SideBar>
+        </Drawer>
+      </Router>
+
     )
   }
 }
